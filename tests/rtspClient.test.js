5bc2b66d3730e22912bef1313ee46d21097f4ceb
ovuruska <oguzvuruskaner@eyecuvision.com>
2021-09-24 05:25:53 -0400

    Added connectivity test.

--- tests/rtspClient.test.js
+++ tests/rtspClient.test.js
@@ -4,8 +4,21 @@ const RTSPClient = require("../src/RTSPClient")
 describe("RTSP Client can " , () => {
     
     
-    it("constructable.",() => {
+    it("be constructed.",() => {
 
-        const client = new RTSPClient()
+        const client = new RTSPClient("192.168.0.30",554)
+    })
+
+    it("fetch options.",(done) => {
+        const client = new RTSPClient("192.168.0.30",554)
+        
+        client.connect("/")
+        .then(() => {
+            client.options()
+            .then(() => {
+                client.destroy()
+                done()
+            })
+        })
     })
 })
